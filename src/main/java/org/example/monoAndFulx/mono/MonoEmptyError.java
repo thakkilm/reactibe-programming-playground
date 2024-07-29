4b44b909a4dfdf3b2f46f6fd3c9383274d591f80
thakkilm <thakkilm@mail.gvsu.edu>
2024-07-28 23:11:21 -0400

    Logic to test Mono.empty() and Mono.error()

---
+++ src/main/java/org/example/monoAndFulx/mono/MonoEmptyError.java
@@ -0,0 +1,28 @@
+package org.example.monoAndFulx.mono;
+
+import org.example.defaults.Util;
+import reactor.core.publisher.Mono;
+
+public class MonoEmptyError {
+
+    public static void main(String[] args) {
+
+        //Prints Mahesh
+        getUserName(1).subscribe(Util.subscriber());
+
+        //Received null value and prints nothing
+        getUserName(2).subscribe(Util.subscriber());
+
+        //Throws error
+        getUserName(3).subscribe(Util.subscriber());
+    }
+
+    public static Mono<String> getUserName(int id){
+        return switch (id){
+            case 1-> Mono.just("Mahesh");
+            case 2->Mono.empty();
+            default -> Mono.error(new RuntimeException("invalid Argument Error"));
+        };
+
+    }
+}
